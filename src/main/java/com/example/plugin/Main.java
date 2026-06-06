0229f70daca1ee56cb6f96990ea0880b37a7eb5d
takipsilimm <gaming.renelynjoyvaldez@gmail.com>
2026-06-06 19:26:18 +0800

    Create Main.java

---
+++ src/main/java/com/example/plugin/Main.java
@@ -0,0 +1,16 @@
+package com.example.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin disabled!");
+    }
+}
